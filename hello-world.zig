7287b67aa178dfbb47727637c6d870b4dac76749
KP <prasanth.25k@gmail.com>
2021-12-13 08:26:22 +0530

    add info about @import to hello-world

--- hello-world.zig
+++ hello-world.zig
@@ -1,3 +1,7 @@
+// import takes in a file or a package name as an argument
+// and returns a struct type. All declarations that are labelled as pub
+// will end up in the struct type.
+// @import("std") gives access to the standard library.
 const std = @import("std");
 
 pub fn main() void {
